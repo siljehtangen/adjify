403908176f66b11a4db0aeccc6216e5b5ba49418
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-04-21 09:34:18 +0200

    feat: add app colors

---
+++ app/lib/config/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kBg = Color(0xFF101D2E);
+const kSurface = Color(0xFF192C44);
+const kBorder = Color(0xFF284D6E);
+const kText = Color(0xFFEBF4FF);
+const kTextSub = Color(0xFF7DB9D8);
