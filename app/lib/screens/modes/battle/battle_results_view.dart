68da8791a8cedefc9f49ccef6539b0aa273a3705
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-04-24 16:44:11 +0200

    chore: battle results view

--- app/lib/screens/modes/battle/battle_results_view.dart
+++ app/lib/screens/modes/battle/battle_results_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import '../../../config/app_colors.dart';
-import '../../../models/story.dart';
+import '../../../models/battle_entry.dart';
 import 'entry_card.dart';
 
 class BattleResultsView extends StatelessWidget {
