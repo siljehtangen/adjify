997fbb1d321a3f397ab6d11b249e72e4d9d7d342
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-04-24 16:45:11 +0200

    chore: api service

--- app/lib/services/api_service.dart
+++ app/lib/services/api_service.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../config/env.dart';
+import '../models/battle_entry.dart';
+import '../models/chain_segment.dart';
 import '../models/room.dart';
 import '../models/story.dart';
 
